75862627dd16e14bd3d3e17f8f796a3d727dccff
raed <eng.salhab@gmail.com>
2022-06-01 18:50:43 +0300

    Create __ne__  method

--- python/ADT/fractions.py
+++ python/ADT/fractions.py
@@ -82,6 +82,12 @@ class Fraction():
 
         return first_num <= second_num
 
+    def __ne__(self, other):
+        first_num = self.num * other.den
+        second_num = other.num * self.den
+
+        return first_num != second_num
+
     def get_num(self):
         return self.num
 
