c4133e0efe26ed78dfa0470b8333ad422ee7d445
raed <eng.salhab@gmail.com>
2022-06-07 18:48:26 +0300

    Modify balance parenthesis checker for more general

--- python/ADT/parenthesis_checker.py
+++ python/ADT/parenthesis_checker.py
@@ -7,13 +7,15 @@ def par_checker(symbol_string):
 
     while index < len(symbol_string) and balanced:
         symbol = symbol_string[index]
-        if symbol == "(":
+        if symbol in "([{":
             s.push(symbol)
         else:
             if s.is_empty():
                 balanced = False
             else:
-                s.pop()
+                top = s.pop()
+                if not matches(top, symbol):
+                    balanced = False
 
         index += 1
 
@@ -22,7 +24,13 @@ def par_checker(symbol_string):
     else:
         return False
 
+def matches(open, close):
+    opens = "([{"
+    closes = ")]}"
+    return opens.index(open) == closes.index(close)
 
 print(par_checker("()()()"))
 print(par_checker("(()()())"))
 print(par_checker("()(()"))
+print(par_checker("{{([][])}()}"))
+print(par_checker("[{()]"))
