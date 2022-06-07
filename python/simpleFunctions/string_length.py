42f3803cc2d26b438a79cca4e62cf433e6aadbdf
raed <eng.salhab@gmail.com>
2022-06-07 20:53:17 +0300

    Create string_length function

---
+++ python/simpleFunctions/string_length.py
@@ -0,0 +1,9 @@
+def string_length(string):
+    """
+    string_length function take a string as parameter
+    and return the number of chracters in the string
+    """
+    length = 0
+    for char in string:
+        length += 1
+    return length
