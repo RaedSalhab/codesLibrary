9c83c303ec8ee57d40065c5b054572ce9be1e560
raed <eng.salhab@gmail.com>
2022-06-10 10:57:24 +0300

    Create base_converting number from decimal to other type number

---
+++ python/simpleFunctions/base_convering.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.insert(1, '/home/raed/codesLibrary/python/ADT')
+from stack import Stack
+
+def base_converter(dec_number, base):
+    digits = "0123456789ABCDEF"
+    rem_stack = Stack()
+    new_string = ""
+    if dec_number == 0:
+        new_string = "0"
+    while dec_number > 0:
+        rem = dec_number % base
+        rem_stack.push(rem)
+        dec_number = dec_number // base
+
+    while not rem_stack.is_empty():
+        new_string += digits[rem_stack.pop()]
+
+    return new_string
