5bebae3976712e9fb9f5da45cbe4c2ab0c111910
raed <eng.salhab@gmail.com>
2022-06-14 23:29:31 +0300

    Update sliding function

--- FrontEnd/carousel/carousel.js
+++ FrontEnd/carousel/carousel.js
@@ -1,33 +1,33 @@
-let firstSlide = document.getElementById("slide1");
-let secondSlide = document.getElementById("slide2");
-let thirdSlide = document.getElementById("slide3");
-let index = 2;
+let next = 2;
 function sliding(){
-    if(index == 2) {
-        document.getElementById("slide1").style.display = "none";
-        document.getElementById("slide2").style.display = "block";
-        document.getElementById("slide3").style.display = "none";
-        index++
-    } else if (index == 3) {
-        document.getElementById("slide1").style.display = "none";
-        document.getElementById("slide2").style.display = "none";
-        document.getElementById("slide3").style.display = "block";
-        index = 1;
-    } else if (index == 1) {
-        document.getElementById("slide3").style.display = "none";
-        document.getElementById("slide1").style.display = "block";
-        document.getElementById("slide2").style.display = "none";
-        index = 2;
+    let firstSlide = document.getElementById("slide1");
+    let secondSlide = document.getElementById("slide2");
+    let thirdSlide = document.getElementById("slide3");
+    if(next == 2) {
+        firstSlide.style.display = "none";
+        secondSlide.style.display = "block";
+        thirdSlide.style.display = "none";
+        next = 3;
+    } else if (next == 3) {
+        firstSlide.style.display = "none";
+        secondSlide.style.display = "none";
+        thirdSlide.style.display = "block";
+        next = 1;
+    } else if (next == 1) {
+        thirdSlide.style.display = "none";
+        firstSlide.style.display = "block";
+        secondSlide.style.display = "none";
+        next = 2;
     }
 }
 
 function previous() {
-    if(index == 1){
-        index = 2;
-    } else if(index == 2) {
-        index = 3;
-    } else if(index == 3){
-        index = 1;
+    if(next == 1){
+        next = 2;
+    } else if(next == 2) {
+        next = 3;
+    } else if(next == 3){
+        next = 1;
     }
     
     sliding();
