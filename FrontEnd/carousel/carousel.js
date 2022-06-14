9558b1120f5fcfc77ad0dd18cd141ccae4146671
raed <eng.salhab@gmail.com>
2022-06-14 22:59:35 +0300

    Add next and previous arrow

--- FrontEnd/carousel/carousel.js
+++ FrontEnd/carousel/carousel.js
@@ -1,20 +1,34 @@
 let firstSlide = document.getElementById("slide1");
 let secondSlide = document.getElementById("slide2");
 let thirdSlide = document.getElementById("slide3");
-let index = 0;
+let index = 2;
 function sliding(){
-    console.log("Done");
-    if(index == 0) {
+    if(index == 2) {
         document.getElementById("slide1").style.display = "none";
         document.getElementById("slide2").style.display = "block";
+        document.getElementById("slide3").style.display = "none";
         index++
-    } else if (index == 1) {
+    } else if (index == 3) {
+        document.getElementById("slide1").style.display = "none";
         document.getElementById("slide2").style.display = "none";
         document.getElementById("slide3").style.display = "block";
-        index++
-    } else if (index == 2) {
+        index = 1;
+    } else if (index == 1) {
         document.getElementById("slide3").style.display = "none";
         document.getElementById("slide1").style.display = "block";
-        index = 0
+        document.getElementById("slide2").style.display = "none";
+        index = 2;
     }
+}
+
+function previous() {
+    if(index == 1){
+        index = 2;
+    } else if(index == 2) {
+        index = 3;
+    } else if(index == 3){
+        index = 1;
+    }
+    
+    sliding();
 }
