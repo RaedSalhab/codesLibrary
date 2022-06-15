991ec034fc6f83e12d96336a2a62c55b670d3c37
raed <eng.salhab@gmail.com>
2022-06-15 19:50:00 +0300

    Add autoSlider function

--- FrontEnd/carousel/carousel.js
+++ FrontEnd/carousel/carousel.js
@@ -1,34 +1,35 @@
-let next = 2;
-function sliding(){
+let currentIndex = 0;
+let nextIndex;
+
+function slider(cIndex, nIndex){
     let firstSlide = document.getElementById("slide1");
     let secondSlide = document.getElementById("slide2");
     let thirdSlide = document.getElementById("slide3");
-    if(next == 2) {
-        firstSlide.style.display = "none";
-        secondSlide.style.display = "block";
-        thirdSlide.style.display = "none";
-        next = 3;
-    } else if (next == 3) {
-        firstSlide.style.display = "none";
-        secondSlide.style.display = "none";
-        thirdSlide.style.display = "block";
-        next = 1;
-    } else if (next == 1) {
-        thirdSlide.style.display = "none";
-        firstSlide.style.display = "block";
-        secondSlide.style.display = "none";
-        next = 2;
+    let slideList = [firstSlide, secondSlide, thirdSlide];
+    slideList[cIndex].style.display = "none";
+    slideList[nIndex].style.display = "block";
+}
+
+function next() {
+    if(currentIndex < 2) {
+        nextIndex = currentIndex + 1;
+    } else {
+        nextIndex = 0;
     }
+    slider(currentIndex, nextIndex);
+    currentIndex = nextIndex;
 }
 
 function previous() {
-    if(next == 1){
-        next = 2;
-    } else if(next == 2) {
-        next = 3;
-    } else if(next == 3){
-        next = 1;
+    if(currentIndex > 0) {
+        nextIndex = currentIndex - 1;
+    } else {
+        nextIndex = 2;
     }
-    
-    sliding();
+    slider(currentIndex, nextIndex);
+    currentIndex = nextIndex;
+}
+
+function autoSlider() {
+    setInterval(next, 5000);
 }
